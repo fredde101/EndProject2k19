ad601f254602272041a7e54d13a99cd0c003f3e6
InavL <ATB1998@gmail.com>
2019-12-16 12:42:44 +0100

    Initial Commit

---
+++ SlutProjekt2k19/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SlutProjekt2k19.MvcApplication" Language="C#" %>
---
+++ SlutProjekt2k19/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SlutProjekt2k19.Startup))]
+namespace SlutProjekt2k19
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
